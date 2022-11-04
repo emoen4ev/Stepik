c17ab1343580a75924646266ad9600995010d278
Emil Enchev <emoen4ev@gmail.com>
2022-11-04 20:04:28 +0200

    Course is updated, added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 1/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 1/Programming (1)/task.py
@@ -1,1 +1,14 @@
-# put your python code here
+m = float(input())
+h = float(input())
+
+opt_min = 18.5
+opt_max = 25
+
+body_mass_index = m / h ** 2
+
+if body_mass_index < opt_min:
+    print('Недостаточная масса')
+elif body_mass_index > opt_max:
+    print('Избыточная масса')
+else:
+    print('Оптимальная масса')
