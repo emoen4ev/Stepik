651fd4e34840a9afab72643a600607483523a7d0
Emil Enchev <emoen4ev@gmail.com>
2022-11-08 14:14:11 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (2)/task.py
@@ -1,1 +1,5 @@
-# put your python code here
+sequence = list(map(int, input().split()))
+for i in range(1, len(sequence), 2):
+    sequence[i], sequence[i - 1] = sequence[i - 1], sequence[i]
+
+print(*sequence)
