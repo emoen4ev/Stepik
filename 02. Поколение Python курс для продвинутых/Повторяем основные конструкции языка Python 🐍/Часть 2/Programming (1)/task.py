869b5317256ff5c47a34a0de31d69fac1aeab86e
Emil Enchev <emoen4ev@gmail.com>
2022-11-08 14:14:11 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (1)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+numbers = list(map(int, input().split()))
+
+counter = 0
+
+for i in range(1, len(numbers)):
+    if numbers[i] > numbers[i - 1]:
+        counter += 1
+
+print(counter)
