3c572537f67018a5f2b871ae0f6346ee21242e47
Emil Enchev <emoen4ev@gmail.com>
2022-11-08 13:07:56 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 1/Programming (7)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 1/Programming (7)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+n = int(input())
+k = int(input())
+
+sequence = list(range(1, n + 1))
+winner = []
+counter = 1
+
+if k != 1:
+    while len(sequence) > 1:
+        for i in range(len(sequence)):
+            if counter % k != 0:
+                winner.append(sequence[i])
+                counter += 1
+            else:
+                counter = 1
+
+        sequence = winner
+        winner = []
+
+print(sequence[-1])
