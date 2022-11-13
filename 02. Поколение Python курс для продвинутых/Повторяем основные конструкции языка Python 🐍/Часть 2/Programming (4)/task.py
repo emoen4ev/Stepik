4a44ccd961386d58eb8b22c21d475c244b36d49a
Emil Enchev <emoen4ev@gmail.com>
2022-11-13 13:22:17 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (4)/task.py
@@ -1,1 +1,8 @@
-# put your python code here
+sequence = input().split()
+counter = 1
+
+for i in range(len(sequence) - 1):
+    if sequence[i + 1] != sequence[i]:
+        counter += 1
+
+print(counter)
