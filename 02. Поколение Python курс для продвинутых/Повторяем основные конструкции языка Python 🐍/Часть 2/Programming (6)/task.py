bc2aed06bc54716f393e11fbcad97a2cac805072
Emil Enchev <emoen4ev@gmail.com>
2022-11-13 15:23:34 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (6)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+t = input()
+r = input()
+
+if t == r:
+    winner = 'ничья'
+elif (t == 'ножницы' and r == 'бумага') or \
+        (t == 'бумага' and r == 'камень') or \
+        (t == 'камень' and r == 'ножницы'):
+    winner = 'Тимур'
+else:
+    winner = 'Руслан'
+
+print(winner)
