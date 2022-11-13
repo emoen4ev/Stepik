029ce89dacc1231589233530a45918537d8275b4
Emil Enchev <emoen4ev@gmail.com>
2022-11-13 14:34:11 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (5)/task.py
@@ -1,1 +1,22 @@
-# put your python code here
+numbers = int(input())
+sequence = []
+result = 'НЕТ'
+
+for _ in range(numbers):
+    sequence.append(int(input()))
+
+target = int(input())
+
+if target == 0 and 0 in sequence:
+    result = 'ДА'
+else:
+    for ind, value in enumerate(sequence):
+        if value != 0 and target % value == 0:
+            for _, value_1 in enumerate(sequence[ind + 1:]):
+                if value * value_1 == target:
+                    result = 'ДА'
+                    break
+        if result == 'ДА':
+            break
+
+print(result)
