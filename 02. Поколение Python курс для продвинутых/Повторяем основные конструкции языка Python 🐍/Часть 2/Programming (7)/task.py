3ff9f11a9ede4ac8ed1015276a91b2d97a6eb39f
Emil Enchev <emoen4ev@gmail.com>
2022-11-14 19:44:12 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (7)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (7)/task.py
@@ -1,1 +1,15 @@
-# put your python code here
+tr = [input(), input()]
+print(tr)
+
+sequence = ['ножницы', 'бумага', 'камень', 'ящерица', 'Спок']
+
+if tr[0] == tr[1]:
+    winner = 'ничья'
+elif abs(sequence.index(tr[0]) - sequence.index(tr[1])) == 1:
+    pass
+elif abs(sequence.index(tr[0]) - sequence.index(tr[1])) == sequence.index(tr[-1]):
+    pass
+elif abs(sequence.index(tr[0]) - sequence.index(tr[1])) == 2:
+    pass
+
+print(sequence.index(tr[0]))
