40bcf9fc8d4f1b3d9722483b4099cb343eb92554
Emil Enchev <emoen4ev@gmail.com>
2022-11-15 19:37:11 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (9)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (9)/task.py
@@ -1,1 +1,11 @@
-# put your python code here
+number = int(input())
+
+sequence = [input() for _ in range(number)]
+
+initial = 'anton'
+
+for word in sequence:
+    ind = 0
+    for ch in word:
+        if ch == initial[ind]:
+            pass
