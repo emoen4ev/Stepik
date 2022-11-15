9edf1ead2aa9213325258410c90e0fbf7fc71793
Emil Enchev <emoen4ev@gmail.com>
2022-11-15 19:11:44 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (8)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (8)/task.py
@@ -1,1 +1,14 @@
-# put your python code here
+text = input()
+
+counter = 0
+result = 0
+
+for ch in text:
+    if ch == 'Р':
+        counter += 1
+    else:
+        if counter > result:
+            result = counter
+        counter = 0
+
+print(max(result, counter))
