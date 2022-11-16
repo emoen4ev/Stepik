63be84584a2075039f1e777afd61099321527dc4
Emil Enchev <emoen4ev@gmail.com>
2022-11-16 09:03:40 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (9)/task.py
+++ 02. Поколение Python курс для продвинутых/Повторяем основные конструкции языка Python 🐍/Часть 2/Programming (9)/task.py
@@ -3,9 +3,20 @@ number = int(input())
 sequence = [input() for _ in range(number)]
 
 initial = 'anton'
+counter = 0
+stop_counter = False
 
 for word in sequence:
     ind = 0
     for ch in word:
         if ch == initial[ind]:
-            pass
+            counter += 1
+            ind += 1
+            if counter == len(initial):
+                stop_counter = True
+
+        if stop_counter:
+            print(sequence.index(word) + 1, end=' ')
+            counter = 0
+            stop_counter = False
+            break
