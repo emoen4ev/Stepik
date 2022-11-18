339688ea79b8adc0f20f05353a2321a06500cd6d
Emil Enchev <emoen4ev@gmail.com>
2022-11-18 13:12:08 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (1)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+n = int(input())
+
+#  -------------------------------------------------------------
+
+# my_list = []
+#
+# numbers = [int(x) for x in range(1, n + 1)]
+#
+# for number in numbers:
+#     current_list = [x for x in range(1, number + 1)]
+#     my_list.append(current_list)
+#
+# for row in my_list:
+#     print(row, end='\n')
+
+#  ---------------------------------------------------------------
+
+my_list = [[x for x in range(1, y + 1)] for y in range(1, n + 1)]
+
+print(*my_list, sep='\n')
--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+n = int(input())
+
+my_list = [[x for x in range(1, n + 1)] for _ in range(n)]
+
+for row in my_list:
+    print(row, end='\n')
