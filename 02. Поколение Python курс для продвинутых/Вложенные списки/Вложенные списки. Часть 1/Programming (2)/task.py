cae2091d85faae9d65a8cdfde2f019f42902b941
Emil Enchev <emoen4ev@gmail.com>
2022-11-18 11:25:38 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 1/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 1/Programming (2)/task.py
@@ -1,4 +1,9 @@
 list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
 maximum = -1
 
+for el in list1:
+    current_maximum = max(el)
+    if current_maximum > maximum:
+        maximum = current_maximum
+
 print(maximum)
--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 1/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 1/Programming (4)/task.py
@@ -1,3 +1,11 @@
 list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
 total = 0
 counter = 0
+
+for el in list1:
+    total += sum(el)
+    counter += len(el)
+
+result = total / counter
+
+print(result)
