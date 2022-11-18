5597d85c81a6b823394a9f108dcee8c1ce90456f
Emil Enchev <emoen4ev@gmail.com>
2022-11-18 15:12:19 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (2)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+def pascal(number):
+    triangle = []
+
+    for i in range(number + 1):
+        row = [1] * (i + 1)
+        for j in range(i + 1):
+            if j not in (0, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    # for row in triangle:
+    #     print(row)
+
+    return triangle[number]
+
+
+n = int(input())
+
+print(pascal(n))
--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (3)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (3)/task.py
@@ -1,1 +1,16 @@
-# put your python code here
+def pascal(number):
+    triangle = []
+
+    for i in range(number):
+        row = [1] * (i + 1)
+        for j in range(i + 1):
+            if j not in (0, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+        print(*row, end='\n')
+
+
+n = int(input())
+
+pascal(n)
