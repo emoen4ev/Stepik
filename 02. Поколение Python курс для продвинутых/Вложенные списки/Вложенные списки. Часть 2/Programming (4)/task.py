b56b908881f14f4cccc3353ce14535af013703c6
Emil Enchev <emoen4ev@gmail.com>
2022-11-18 16:22:19 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Вложенные списки. Часть 2/Programming (4)/task.py
@@ -1,1 +1,15 @@
-# put your python code here
+text = input().split()
+
+result = []
+current = []
+
+for el in text:
+    if not current or el in current:
+        current.append(el)
+    else:
+        result.append(current)
+        current = [el]
+
+result.append(current)
+
+print(result)
