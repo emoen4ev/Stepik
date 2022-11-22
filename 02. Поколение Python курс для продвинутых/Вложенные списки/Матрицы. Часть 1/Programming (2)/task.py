a43f97dd1d357da30302700992e032bf0d4b3187
Emil Enchev <emoen4ev@gmail.com>
2022-11-22 11:31:58 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming (2)/task.py
@@ -1,15 +1,18 @@
 rows_cols = int(input())
 
-matrix = []
-
-for _ in range(rows_cols):
-    curr_row = [int(x) for x in input().split()]
-
-    matrix.append(curr_row)
-
 tr = 0
 
+# matrix = []
+#
+# for _ in range(rows_cols):
+#     curr_row = [int(x) for x in input().split()]
+#
+#     matrix.append(curr_row)
+#
+# for i in range(rows_cols):
+#     tr += matrix[i][i]
+
 for i in range(rows_cols):
-    tr += matrix[i][i]
+    tr += int(input().split()[i])
 
 print(tr)
