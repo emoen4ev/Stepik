5cf4c89fea89aab95e8e6c4e793a39e72a491382
Emil Enchev <emoen4ev@gmail.com>
2022-11-22 10:26:58 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+n, m = int(input()), int(input())
+
+matrix = []
+
+for _ in range(n):
+    row = [input() for _ in range(m)]
+    # row = []
+    # for _ in range(m):
+    #     row.append(input())
+    matrix.append(row)
+
+for row in matrix:
+    print(*row)
