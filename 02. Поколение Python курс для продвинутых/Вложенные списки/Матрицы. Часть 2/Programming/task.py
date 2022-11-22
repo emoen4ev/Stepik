d53e8afe39fdf2f9356c86fb7a6963f32841be6a
Emil Enchev <emoen4ev@gmail.com>
2022-11-22 16:38:57 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming/task.py
@@ -1,1 +1,12 @@
-# put your python code here
+rows, cols = int(input()), int(input())
+
+matrix = [[0] * cols for _ in range(rows)]
+
+for i in range(rows):
+    for j in range(cols):
+        matrix[i][j] = i * j
+
+for row in matrix:
+    for col in row:
+        print(str(col).ljust(3), end='')
+    print()
