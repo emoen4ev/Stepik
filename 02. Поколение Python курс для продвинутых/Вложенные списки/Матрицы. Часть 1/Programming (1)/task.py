f32b07c571fb259cb2abb0d001c0dfa3ed47d47a
Emil Enchev <emoen4ev@gmail.com>
2022-11-22 10:49:44 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 1/Programming (1)/task.py
@@ -1,1 +1,18 @@
-# put your python code here
+rows, cols = int(input()), int(input())
+
+matrix = []
+
+for _ in range(rows):
+    current_row = [input() for _ in range(cols)]
+
+    matrix.append(current_row)
+
+for row in matrix:
+    print(*row)
+
+print()
+
+for col in range(cols):
+    for row in matrix:
+        print(row[col], end=' ')
+    print()
