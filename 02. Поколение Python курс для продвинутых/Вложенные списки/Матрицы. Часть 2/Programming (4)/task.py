00dee0d5103fe7e459fdde76989a84c2c40cd238
Emil Enchev <emoen4ev@gmail.com>
2022-11-23 19:31:09 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming (4)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+rows_cols = int(input())
+
+matrix = [input().split() for _ in range(rows_cols)]
+
+for i in range(rows_cols):
+    j = rows_cols - 1 - i
+    matrix[i][i], matrix[j][i] = matrix[j][i], matrix[i][i]
+
+for row in matrix:
+    print(*row)
