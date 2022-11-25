16e30df10db53200e4526be49418a5e095b053dd
Emil Enchev <emoen4ev@gmail.com>
2022-11-25 10:42:10 +0200

    Added solution, course updated.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming (7)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 2/Programming (7)/task.py
@@ -1,7 +1,8 @@
-board = [['.'] * 8 for _ in range(8)]
+fields = 8
+board = [['.'] * fields for _ in range(fields)]
 
 horse_coordinates = input()
-horse_row = 8 - int(horse_coordinates[1])
+horse_row = fields - int(horse_coordinates[1])
 horse_col = horse_coordinates[0]
 
 horse_col_digit = 0
@@ -24,25 +25,28 @@ for el in map_table:
 
 board[horse_row][horse_col_digit] = 'N'
 
-if horse_row - 2 >= 0:
-    if horse_col_digit - 1 >= 0:
+upper_board_limit = left_board_limit = 0
+lower_board_limit = right_board_limit = fields - 1
+
+if horse_row - 2 >= upper_board_limit:
+    if horse_col_digit - 1 >= left_board_limit:
         board[horse_row - 2][horse_col_digit - 1] = '*'
-    if horse_col_digit + 1 <= 7:
+    if horse_col_digit + 1 <= right_board_limit:
         board[horse_row - 2][horse_col_digit + 1] = '*'
-if horse_row - 1 >= 0:
-    if horse_col_digit - 2 >= 0:
+if horse_row - 1 >= upper_board_limit:
+    if horse_col_digit - 2 >= left_board_limit:
         board[horse_row - 1][horse_col_digit - 2] = '*'
-    if horse_col_digit + 2 <= 7:
+    if horse_col_digit + 2 <= right_board_limit:
         board[horse_row - 1][horse_col_digit + 2] = '*'
-if horse_row + 2 <= 7:
-    if horse_col_digit - 1 >= 0:
+if horse_row + 2 <= lower_board_limit:
+    if horse_col_digit - 1 >= left_board_limit:
         board[horse_row + 2][horse_col_digit - 1] = '*'
-    if horse_col_digit + 1 <= 7:
+    if horse_col_digit + 1 <= right_board_limit:
         board[horse_row + 2][horse_col_digit + 1] = '*'
-if horse_row + 1 <= 7:
-    if horse_col_digit - 2 >= 0:
+if horse_row + 1 <= lower_board_limit:
+    if horse_col_digit - 2 >= left_board_limit:
         board[horse_row + 1][horse_col_digit - 2] = '*'
-    if horse_col_digit + 2 <= 7:
+    if horse_col_digit + 2 <= right_board_limit:
         board[horse_row + 1][horse_col_digit + 2] = '*'
 
 for row in board:
