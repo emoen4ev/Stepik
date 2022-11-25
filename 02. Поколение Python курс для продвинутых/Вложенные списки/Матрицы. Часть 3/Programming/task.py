7f622b4c922252136245648c7f4eab4202ecfd8e
Emil Enchev <emoen4ev@gmail.com>
2022-11-25 13:57:25 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 3/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 3/Programming/task.py
@@ -1,1 +1,23 @@
-# put your python code here
+rows, cols = map(int, input().split())
+
+matrix = []
+
+for i in range(rows):
+    current_row = []
+
+    if i % 2 == 1:
+        flag = False
+    else:
+        flag = True
+
+    for _ in range(cols):
+        if flag:
+            current_row.append('.')
+        else:
+            current_row.append('*')
+        flag = not flag
+
+    matrix.append(current_row)
+
+for row in matrix:
+    print(*row)
