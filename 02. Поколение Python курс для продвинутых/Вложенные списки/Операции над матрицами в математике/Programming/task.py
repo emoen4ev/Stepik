6f85807d7c2d67e8baaf41fcc557263cdad65e15
Emil Enchev <emoen4ev@gmail.com>
2022-11-28 19:16:14 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Операции над матрицами в математике/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Операции над матрицами в математике/Programming/task.py
@@ -1,1 +1,18 @@
-# put your python code here
+rows, cols = [int(_) for _ in input().split()]
+
+matrix_1 = [[int(_) for _ in input().split()] for _ in range(rows)]
+
+input()
+
+matrix_2 = [[int(_) for _ in input().split()] for _ in range(rows)]
+
+
+def get_value(row, col):
+    return matrix_1[row][col] + matrix_2[row][col]
+
+
+result = [[get_value(i, j) for j in range(cols)] for i in range(rows)]
+
+for line in result:
+    line = ' '.join(str(_) for _ in line)
+    print(line)
