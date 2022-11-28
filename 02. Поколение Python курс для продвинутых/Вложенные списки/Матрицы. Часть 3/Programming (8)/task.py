e5ffba0d3bddec13822ee3c061c2a6b0511fe101
Emil Enchev <emoen4ev@gmail.com>
2022-11-28 13:07:56 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 3/Programming (8)/task.py
+++ 02. Поколение Python курс для продвинутых/Вложенные списки/Матрицы. Часть 3/Programming (8)/task.py
@@ -2,14 +2,14 @@ rows, cols = [int(_) for _ in input().split()]
 
 matrix = [[1] * cols for _ in range(rows)]
 
-counter = 0
+value = 0
 
 for k in range(rows * cols):
-    for i in range(rows):
-        for j in range(cols):
-            if i + j == k:
-                counter += 1
-                matrix[i][j] = counter
+    for cur_row in range(rows):
+        for cur_col in range(cols):
+            if cur_row + cur_col == k:
+                value += 1
+                matrix[cur_row][cur_col] = value
 
 for row in matrix:
     row = ''.join(str(_).ljust(3) for _ in row)
