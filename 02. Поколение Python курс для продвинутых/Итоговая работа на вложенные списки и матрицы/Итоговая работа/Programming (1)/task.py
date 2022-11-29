1453490b024e93832efe4560941d6fabe68b0aaa
Emil Enchev <emoen4ev@gmail.com>
2022-11-29 18:38:47 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (1)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+rows_cols = int(input())
+
+matrix = [[int(_) for _ in input().split()] for _ in range(rows_cols)]
+
+largest = matrix[rows_cols - 1][rows_cols - 1]
+
+for i in range(rows_cols):
+    k = rows_cols - i - 2
+    for j in range(rows_cols - 1, k, -1):
+        if matrix[i][j] > largest:
+            largest = matrix[i][j]
+
+print(largest)
