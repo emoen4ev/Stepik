2066086a319c2eb85ad4ad060b2ccbb4e060274b
Emil Enchev <emoen4ev@gmail.com>
2022-11-30 15:05:21 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (2)/task.py
@@ -4,9 +4,9 @@ matrix = [[int(_) for _ in input().split()] for _ in range(rows_cols)]
 
 result = [[0 for _ in range(rows_cols)] for _ in range(rows_cols)]
 
-for i in range(rows_cols):
-    for j in range(rows_cols):
-        result[j][i] = matrix[i][j]
+for cur_row in range(rows_cols):
+    for cur_col in range(rows_cols):
+        result[cur_col][cur_row] = matrix[cur_row][cur_col]
 
 for line in result:
     print(*line)
