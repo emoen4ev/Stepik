bec58800577dab07bf5a84915eab52ac22fdc83a
Emil Enchev <emoen4ev@gmail.com>
2022-11-30 19:02:39 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (11)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (11)/task.py
@@ -1,4 +1,7 @@
 tuples = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'), (1,), (), (), ('d',), ('', ''), ()]
-non_empty_tuples = 
+
+# non_empty_tuples = [tuples[i] for i in range(len(tuples)) if tuples[i]]
+
+non_empty_tuples = [el for el in tuples if el]
 
 print(non_empty_tuples)
--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (12)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (12)/task.py
@@ -1,3 +1,9 @@
 tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
-new_tuples = 
+
+new_tuples = []
+
+for el in tuples:
+    new_el = el[:-1] + (100,)
+    new_tuples.append(new_el)
+
 print(new_tuples)
