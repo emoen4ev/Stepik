1426be45165e521fdc2a459300f0a9d865d3a1e6
Emil Enchev <emoen4ev@gmail.com>
2022-11-30 11:49:19 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на вложенные списки и матрицы/Итоговая работа/Programming (4)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+n = int(input())
+
+matrix = [input().split() for _ in range(n)]
+
+result = 'YES'
+
+for i in range(n):
+    k = n - i - 1
+
+    for j in range(k):
+        r = n - j - 1
+
+        if matrix[i][j] != matrix[r][k]:
+            result = 'NO'
+            break
+
+    if result == 'NO':
+        break
+
+print(result)
