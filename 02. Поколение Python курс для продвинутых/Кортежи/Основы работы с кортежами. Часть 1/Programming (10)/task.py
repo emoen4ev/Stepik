c64f929d03127d0547d65c0b914a1548347d983a
Emil Enchev <emoen4ev@gmail.com>
2022-11-30 18:29:32 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (10)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (10)/task.py
@@ -1,4 +1,4 @@
 city_name = input()
 city_year = int(input())
-city = 
+city = (city_name, city_year)
 print(city)
--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (9)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 1/Programming (9)/task.py
@@ -2,4 +2,6 @@ numbers1 = (1, 2, 3)
 numbers2 = (6,)
 numbers3 = (7, 8, 9, 10, 11, 12, 13)
 
-print()
+result = numbers1 * 2 + numbers2 * 9 + numbers3
+
+print(result)
