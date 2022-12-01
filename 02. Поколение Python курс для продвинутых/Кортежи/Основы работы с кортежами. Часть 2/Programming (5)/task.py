e5d07ffd2c42333e3e94a6142cfd05dfb81b459a
Emil Enchev <emoen4ev@gmail.com>
2022-12-01 18:43:16 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Programming (5)/task.py
@@ -1,1 +1,16 @@
-# put your python code here
+n = int(input())
+
+listing = ()
+
+for _ in range(n):
+    cur = tuple(input().split())
+    listing += cur,
+
+for el in listing:
+    print(*el, end='\n')
+
+print()
+
+for el in listing:
+    if int(el[1]) > 3:
+        print(*el, end='\n')
