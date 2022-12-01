98624add909feb2207a436843df4ab80207aafed
Emil Enchev <emoen4ev@gmail.com>
2022-12-01 18:09:56 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Programming (4)/task.py
@@ -1,1 +1,10 @@
-# put your python code here
+def get_coordinates(param_1, param_2, param_3):
+    x = -param_2 / (2 * param_1)
+    y = (4 * param_1 * param_3 - param_2 ** 2) / (4 * param_1)
+
+    return x, y
+
+
+a, b, c = int(input()), int(input()), int(input())
+
+print(get_coordinates(a, b, c))
