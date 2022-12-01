a3b4c053d50d915bcf68722de097b7468cd51b1e
Emil Enchev <emoen4ev@gmail.com>
2022-12-01 13:46:15 +0200

    Added solution.

--- 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Quiz (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Кортежи/Основы работы с кортежами. Часть 2/Quiz (1)/task.py
@@ -1,2 +1,14 @@
-#  You can experiment here, it won’t be checked
+poets = [
+    ('Тургенев', 14),
+    ('Есенин', 13),
+    ('Маяковский', 28),
+    ('Фет', 15),
+    ('Лермонтов', 20)]
 
+for i in range(len(poets)):
+    for j in range(i+1, len(poets)):
+        if poets[i] > poets[j]:
+            poets[i], poets[j] = poets[j], poets[i]
+
+print(poets[0])
+print(poets[-1])
