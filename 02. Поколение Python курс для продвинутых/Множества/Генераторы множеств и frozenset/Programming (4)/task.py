93ff548e2776ba15441e697127cb9a6d0c343435
Emil Enchev <emoen4ev@gmail.com>
2022-12-11 19:20:42 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Programming (4)/task.py
@@ -1,1 +1,9 @@
-files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT',
+         'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py',
+         'stepik.org', 'kotlin.ko', 'github.git']
+
+my_set = {group.lower() for group in files}
+
+result = {group for group in my_set if group.split('.')[1] == 'png'}
+
+print(*sorted(result))
