4e2b4db414ec2b5e7aed8f35967e7db58b3ec233
Emil Enchev <emoen4ev@gmail.com>
2022-12-11 16:05:29 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Множества/Методы множеств. Часть 3/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Множества/Методы множеств. Часть 3/Programming (4)/task.py
@@ -2,7 +2,7 @@ grades_student_1 = set(input().split(' '))
 grades_student_2 = set(input().split(' '))
 grades_student_3 = set(input().split(' '))
 
-result = grades_student_3.difference(grades_student_1,grades_student_2)
+result = grades_student_3.difference(grades_student_1, grades_student_2)
 
 result = sorted((int(_) for _ in result), reverse=True)
 
