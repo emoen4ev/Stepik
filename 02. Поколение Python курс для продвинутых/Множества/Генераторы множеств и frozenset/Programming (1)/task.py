7bf8e45d231b15121828bc125d4e1548820a7295
Emil Enchev <emoen4ev@gmail.com>
2022-12-11 18:25:44 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Programming (1)/task.py
@@ -1,1 +1,6 @@
-words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon',
+         'tangerine', 'Watermelon', 'currant', 'Almond']
+
+my_set = {word[0].lower() for word in words}
+
+print(*sorted(my_set))
--- 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Множества/Генераторы множеств и frozenset/Theory/task.py
@@ -1,2 +1,5 @@
+digits = {int(c) for c in input()}
+digits_1 = set(int(c) for c in input())
 
-
+print(*digits)
+print(*digits_1)
