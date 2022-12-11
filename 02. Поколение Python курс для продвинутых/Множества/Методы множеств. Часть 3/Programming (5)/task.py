4e403c869545c155a35eff6a38a1a07eb3252fbd
Emil Enchev <emoen4ev@gmail.com>
2022-12-11 16:32:12 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Множества/Методы множеств. Часть 3/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Множества/Методы множеств. Часть 3/Programming (5)/task.py
@@ -2,7 +2,10 @@ grades_student_1, grades_student_2, grades_student_3 = [set(map(int, input().spl
 
 grades_for_all = grades_student_1.union(grades_student_2, grades_student_3)
 
-possible_grades = set(_ for _ in range(11))
+min_grade = 0
+max_grade = 10
+
+possible_grades = set(range(min_grade, max_grade + 1))
 
 result = possible_grades.difference(grades_for_all)
 
