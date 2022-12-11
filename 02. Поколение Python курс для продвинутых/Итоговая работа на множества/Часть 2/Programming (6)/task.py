5bb8505317bd728ff06ca8f2f10bf3892d1de54d
Emil Enchev <emoen4ev@gmail.com>
2022-12-12 01:50:18 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (6)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+m = int(input())
+n = int(input())
+
+math_set = {input() for _ in range(m)}
+info_set = {input() for _ in range(n)}
+
+only_math = math_set.difference(info_set)
+
+print(len(only_math))
