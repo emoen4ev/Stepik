1de72cc45a793af4cc5b49092b5a755d597fbaf0
Emil Enchev <emoen4ev@gmail.com>
2022-12-12 18:52:13 +0200

    Added solutions.

---
+++ 02. Поколение Python курс для продвинутых/Словари/Основы работы со словарями/Programming (3)/task.py
@@ -0,0 +1,15 @@
+my_map = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+}
+
+for digit in input():
+    print(my_map[digit], end=' ')
--- 02. Поколение Python курс для продвинутых/Словари/Основы работы со словарями/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Словари/Основы работы со словарями/Programming (5)/task.py
@@ -1,1 +1,22 @@
-# put your python code here
+text = input()
+
+key_map = {
+    '1': '.,?!:',
+    '2': 'ABC',
+    '3': 'DEF',
+    '4': 'GHI',
+    '5': 'JKL',
+    '6': 'MNO',
+    '7': 'PQRS',
+    '8': 'TUV',
+    '9': 'WXYZ',
+    '0': ' ',
+}
+
+for symbol in text:
+    for key, value in key_map.items():
+        symbol = symbol.upper()
+        if symbol in value:
+            rp = value.index(symbol) + 1
+            print(key * rp, end='')
+            break
