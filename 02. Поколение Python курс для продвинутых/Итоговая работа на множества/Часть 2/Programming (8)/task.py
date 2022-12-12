35773fb430158fadcaeeebd57e96db13c197662f
Emil Enchev <emoen4ev@gmail.com>
2022-12-12 02:35:43 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (8)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (8)/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+set_1 = {_ for _ in input().split()}
+set_2 = {_ for _ in input().split()}
+
+set_3 = set_1.union(set_2)
+
+print(*sorted(set_3))
--- 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (9)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на множества/Часть 2/Programming (9)/task.py
@@ -1,1 +1,8 @@
-# put your python code here
+m, n = int(input()), int(input())
+
+set_m = {input() for _ in range(m)}
+set_n = {input() for _ in range(n)}
+
+result = len(set_m.symmetric_difference(set_n))
+
+print(result if result else 'NO')
