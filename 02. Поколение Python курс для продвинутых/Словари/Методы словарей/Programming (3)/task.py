6c57f5ca0e6bdb85141b222fdcc9ba586ddd27bc
Emil Enchev <emoen4ev@gmail.com>
2022-12-13 18:10:45 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Словари/Методы словарей/Programming (3)/task.py
+++ 02. Поколение Python курс для продвинутых/Словари/Методы словарей/Programming (3)/task.py
@@ -1,1 +1,23 @@
-s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange' \
+    ' barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot' \
+    ' currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon' \
+    ' pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple' \
+    ' barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit' \
+    ' banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+s = s.split()
+
+result = {}
+
+for word in s:
+    if word not in result:
+        result[word] = s.count(word)
+
+new = sorted(result, key=lambda x: (max(x[1]), x[0]))
+
+print(max(sorted(result, key=lambda x: (x[1], x[0]))))
+
+
+print(result)
+
+print(new)
