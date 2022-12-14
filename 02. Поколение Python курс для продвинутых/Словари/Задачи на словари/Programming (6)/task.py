8218b4b3c596ff4a39618c8b08dcf2470a3077c7
Emil Enchev <emoen4ev@gmail.com>
2022-12-14 19:46:03 +0200

    Added solutions.

---
+++ 02. Поколение Python курс для продвинутых/Словари/Задачи на словари/Programming (6)/task.py
@@ -0,0 +1,39 @@
+word = input()
+n = int(input())
+
+chars_data = {}
+word_data = {}
+result = {}
+
+for _ in range(n):
+    ch, number = input().split(': ')
+    chars_data.setdefault(ch, int(number))
+
+chars_data_sorted = sorted(chars_data.items(), key=lambda x: x[1])
+
+for symbol in word:
+    if symbol in word_data:
+        continue
+    word_data.setdefault(symbol, word.count(symbol))
+
+word_data_sorted = sorted(word_data.items(), key=lambda x: x[1])
+
+for i in range(len(chars_data_sorted)):
+    k = word_data_sorted[i][0]
+    v = chars_data_sorted[i][0]
+    result.setdefault(k, v)
+
+for el in word:
+    print(result[el], end='')
+
+# ----------------------------------------------------------------
+
+# dct, word = {}, {}
+# s = input()
+# for c in s:
+#     word[c] = word.get(c, 0) + 1
+# for _ in range(int(input())):
+#     a, b = input().split(': ')
+#     dct[int(b)] = a
+# for c in s:
+#     print(dct[word[c]], end='')
