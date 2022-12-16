00ccce2b122ed37b4e938e46b8238316de3cfc5e
Emil Enchev <emoen4ev@gmail.com>
2022-12-16 11:03:39 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на словари/Часть 1/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на словари/Часть 1/Programming (1)/task.py
@@ -1,6 +1,8 @@
-emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'], 
-          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'], 
-          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'], 
-          'yandex.ru': ['surface', 'google'],
-          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
-          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
+emails = {
+    'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'],
+    'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'],
+    'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'],
+    'yandex.ru': ['surface', 'google'],
+    'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
+    'mail.ru': ['angel.down', 'joanne', 'the.fame.moster'],
+}
