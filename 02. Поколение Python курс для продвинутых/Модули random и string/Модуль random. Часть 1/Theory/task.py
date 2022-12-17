ef2a43a70e2c3e0cba1aea6f759b37d8edc7b917
Emil Enchev <emoen4ev@gmail.com>
2022-12-17 19:54:06 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 1/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 1/Theory/task.py
@@ -1,2 +1,14 @@
-#  You can experiment here, it won’t be checked
+import random
 
+print(random.randint(-2, 9))
+print(random.randrange(-2, 9))
+
+print(random.random())
+
+c = random.uniform(-5, 5)
+print(c)
+
+random.seed(17)   # явно устанавливаем начальное значение для генератора случайных чисел
+
+for _ in range(10):
+    print(random.randint(1, 100))
