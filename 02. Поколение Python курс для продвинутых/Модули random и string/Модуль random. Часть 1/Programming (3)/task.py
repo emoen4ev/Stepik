086b377f727549e5d172121cb81f97a6a54ad020
Emil Enchev <emoen4ev@gmail.com>
2022-12-19 16:51:51 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 1/Programming (3)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 1/Programming (3)/task.py
@@ -3,9 +3,11 @@ from random import randint
 password = set()
 
 password_length = 7
+min_limit = 1
+max_limit = 49
 
 while len(password) < password_length:
-    new_digit = randint(1, 49)
+    new_digit = randint(min_limit, max_limit)
     password.add(new_digit)
 
 password = sorted(password)
