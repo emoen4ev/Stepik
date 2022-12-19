bd5ccb8be2797a91b02e1c7509e91169806bad35
Emil Enchev <emoen4ev@gmail.com>
2022-12-19 17:56:35 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Theory/task.py
@@ -1,2 +1,26 @@
-#  You can experiment here, it won’t be checked
+import random
 
+l = ['jj', 1, 5, False]
+
+t = ('r', True, 4, 'fgh')
+
+s = 'SDFGDH'
+
+x = {2, 5, 'hgf'}
+
+random.shuffle(l)
+
+print(l)
+
+ll = random.choice(l)
+tt = random.choice(t)
+ss = random.choice(s)
+
+
+print(ll)
+print(tt)
+print(ss)
+
+print(type(ll))
+print(type(tt))
+print(type(ss))
