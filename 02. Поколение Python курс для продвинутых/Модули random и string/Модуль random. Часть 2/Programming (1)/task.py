f6da45767c21e9b891154010d3f9d35c62972361
Emil Enchev <emoen4ev@gmail.com>
2022-12-21 09:43:37 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (1)/task.py
@@ -1,2 +1,9 @@
+from string import ascii_uppercase as uc
+from random import randint as rt, sample as sm
+
+
 def generate_index():
-    pass
+    return f'{"".join(sm(uc, 2))}{rt(0, 99)}_{rt(0, 99)}{"".join(sm(uc, 2))}'
+
+
+# print(generate_index())
