82774886308e6ee884f10b485f3c626390b3a14e
Emil Enchev <emoen4ev@gmail.com>
2022-12-27 00:36:54 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (7)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (7)/task.py
@@ -1,7 +1,18 @@
+from string import ascii_letters, digits
+from random import choices
+
+
 def generate_password(length):
-    pass
+    return choices(base_sequence, k=length)
+
 
 def generate_passwords(count, length):
-    pass
+    for _ in range(count):
+        print(''.join(generate_password(length)))
+
 
 n, m = int(input()), int(input())
+
+base_sequence = list((set(ascii_letters) | set(digits)) - {'l', 'I', '1', 'o', 'O', '0'})
+
+generate_passwords(n, m)
