fab3d15d7f90c2c956d4dfe751b4181ec728e12e
Emil Enchev <emoen4ev@gmail.com>
2022-12-27 13:49:17 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Метод Монте-Карло и Bogosort/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Метод Монте-Карло и Bogosort/Programming (1)/task.py
@@ -1,3 +1,20 @@
-import random
+from random import uniform
 
-n = 10**6       # количество испытаний
+n = 10 ** 6  # количество испытаний
+
+x_0 = 2
+y_0 = 2
+s_0 = x_0 * y_0
+
+k = 0
+
+for _ in range(n):
+    x = uniform(-1, 1)
+    y = uniform(-1, 1)
+
+    if x ** 2 + y ** 2 - 1 <= 0:
+        k += 1
+
+s = (k / n) * s_0
+
+print(s)
--- 02. Поколение Python курс для продвинутых/Модули random и string/Метод Монте-Карло и Bogosort/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Метод Монте-Карло и Bogosort/Programming/task.py
@@ -1,3 +1,20 @@
-import random
+from random import uniform
 
-n = 10**6       # количество испытаний
+n = 10 ** 6  # количество испытаний
+
+x_0 = 4
+y_0 = 4
+s_0 = x_0 * y_0
+
+k = 0
+
+for _ in range(n):
+    x = uniform(-2, 2)
+    y = uniform(-2, 2)
+
+    if 3 * x + y ** 2 - 2 <= 0 <= x ** 3 + y ** 4 + 2:
+        k += 1
+
+s = (k / n) * s_0
+
+print(s)
