d125c4fec9fc3f33f5adaac14ffea47bf802c1e4
Emil Enchev <emoen4ev@gmail.com>
2022-12-27 03:26:54 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (8)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули random и string/Модуль random. Часть 2/Programming (8)/task.py
@@ -1,7 +1,23 @@
+from string import ascii_uppercase, ascii_lowercase, digits
+from random import choice, choices, shuffle
+
+
 def generate_password(length):
-    pass
+    psw = [choice(uppercase_set), choice(lowercase_set), choice(digits_set)]
+    psw.extend(choices(base_sequence, k=(length - 3)))
+    shuffle(psw)
+    return ''.join(psw)
+
 
 def generate_passwords(count, length):
-    pass
+    return [generate_password(length) for _ in range(count)]
+
 
 n, m = int(input()), int(input())
+
+uppercase_set = list(set(ascii_uppercase) - {'O', 'I'})
+lowercase_set = list(set(ascii_lowercase) - {'l', 'o'})
+digits_set = list(set(digits) - {'1', '0'})
+base_sequence = uppercase_set + lowercase_set + digits_set
+
+print(*generate_passwords(n, m), sep='\n')
