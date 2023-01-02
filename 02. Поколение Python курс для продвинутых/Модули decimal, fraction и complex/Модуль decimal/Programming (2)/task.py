bc35df2da641464ff0c20ab564949776fb35f020
Emil Enchev <emoen4ev@gmail.com>
2023-01-02 18:07:13 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Модуль decimal/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Модуль decimal/Programming (2)/task.py
@@ -1,2 +1,22 @@
 from decimal import *
+
+
 num = Decimal(input())
+
+num_tuple = num.as_tuple()
+
+num_digits = num_tuple.digits
+
+if len(num_digits) == abs(num_tuple.exponent):
+    num_digits += (0,)
+# elif num_tuple.sign == 1:
+#     num_digits = list(num_digits)
+#     first_digit = num_digits.pop(0)
+#     num_digits.append(-first_digit)
+
+smallest_digit = min(num_digits)
+largest_digit = max(num_digits)
+
+result = smallest_digit + largest_digit
+
+print(result)
