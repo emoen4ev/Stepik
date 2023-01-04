62ec27c93a37f3cc3e27cadbbbbd3807f98d8c4d
Emil Enchev <emoen4ev@gmail.com>
2023-01-04 16:27:06 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Модуль fractions/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Модуль fractions/Programming (6)/task.py
@@ -7,9 +7,14 @@ n = int(input())
 sequence = list()
 
 for num in range(1, n // 2):
-    cur_num = num / (n - num)
+    cur_num = Fr(num, n - num)
     sequence.append(cur_num)
 
-
-result = Fr(str(max(sequence)))
-print(result)
+sequence.sort(reverse=True)
+largest = max(sequence)
+sequence_2 = [str(x) for x in sequence]
+largest_2 = max(sequence_2)
+print(*sequence)
+print(sequence_2)
+print(largest)
+print(largest_2)
