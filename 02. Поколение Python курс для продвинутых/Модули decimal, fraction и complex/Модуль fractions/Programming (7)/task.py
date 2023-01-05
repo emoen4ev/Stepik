f065ffcb37ffb027fc15a02e67d3a9cb90117646
Emil Enchev <emoen4ev@gmail.com>
2023-01-05 16:13:24 +0200

    Added solutions.

---
+++ 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Модуль fractions/Programming (7)/task.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+
+
+n = int(input())
+
+result = set()
+
+for i in range(1, n):
+    for j in range(i + 1, n + 1):
+        result.add(Fraction(i, j))
+
+result = sorted(tuple(result))
+
+print(*result, sep="\n")
