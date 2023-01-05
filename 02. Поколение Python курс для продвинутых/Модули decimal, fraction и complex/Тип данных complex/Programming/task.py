2ec1e4566f2bc23753eaa6971ec163a85e96c9a3
Emil Enchev <emoen4ev@gmail.com>
2023-01-05 18:12:20 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Тип данных complex/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Модули decimal, fraction и complex/Тип данных complex/Programming/task.py
@@ -1,1 +1,6 @@
-# put your python code here
+compl_1 = input()
+compl_2 = input()
+
+print(f'({compl_1}) + ({compl_2}) = {complex(compl_1) + complex(compl_2)}')
+print(f'({compl_1}) - ({compl_2}) = {complex(compl_1) - complex(compl_2)}')
+print(f'({compl_1}) * ({compl_2}) = {complex(compl_1) * complex(compl_2)}')
