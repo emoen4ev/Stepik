3ecbca7668b51b493741cd2c30246813f9a6c1e8
Emil Enchev <emoen4ev@gmail.com>
2023-01-06 13:42:37 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (1)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (1)/task.py
@@ -1,2 +1,18 @@
+import turtle
 
 
+def rectangle(width, height):
+    turtle.forward(width)
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.left(90)
+    turtle.forward(height)
+
+
+x, y = 200, 100
+
+rectangle(x, y)
+
+turtle.exitonclick()
