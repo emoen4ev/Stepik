f1b63270ef1160973dcb44edaa93d0b67ab96333
Emil Enchev <emoen4ev@gmail.com>
2023-01-06 16:57:14 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (4)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (4)/task.py
@@ -1,2 +1,25 @@
+import turtle as t
 
 
+def draw_4_squares(a,angle):
+    for _ in range(4):
+        t.setheading(angle)
+        for _ in range(4):
+            t.forward(a)
+            t.left(90)
+        angle += 90
+
+
+def draw_many_squares(a, start, end, d):
+    for angle in range(start, end + 1, d):
+        draw_4_squares(a, angle)
+
+
+side = 150
+start_angle = 0
+end_angle = 45
+diff = 45
+
+draw_many_squares(side, start_angle, end_angle, diff)
+
+t.exitonclick()
