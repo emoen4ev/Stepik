29cffd067b33e5228a2c8dadc008494d2f815fc9
Emil Enchev <emoen4ev@gmail.com>
2023-01-06 18:25:52 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (6)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (6)/task.py
@@ -1,2 +1,26 @@
+import turtle as t
 
 
+def draw_regular_polygon(a, b):
+    for _ in range(number_of_sides):
+        t.forward(a)
+        t.right(b)
+    t.forward(a)
+
+
+def draw_polygons(a, b):
+    for _ in range(number_of_sides):
+        draw_regular_polygon(a, b)
+        t.left(b)
+
+
+t.speed(10)
+
+side_length = 100
+number_of_sides = 6
+
+angle = 360 / number_of_sides
+
+draw_polygons(side_length, angle)
+
+t.exitonclick()
