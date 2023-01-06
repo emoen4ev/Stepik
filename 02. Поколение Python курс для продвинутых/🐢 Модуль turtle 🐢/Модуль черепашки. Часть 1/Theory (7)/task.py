267732179d4a35564b9367788c4c270eb32f65dc
Emil Enchev <emoen4ev@gmail.com>
2023-01-06 19:59:40 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (7)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (7)/task.py
@@ -1,2 +1,16 @@
+import turtle as t
 
 
+def draw_rhombus(a, b):
+    for i in range(4):
+        t.forward(a)
+        current_angle = 180 - b if i % 2 == 1 else b
+        t.right(current_angle)
+
+
+side_length = 100
+angle = 60
+
+draw_rhombus(side_length, angle)
+
+t.exitonclick()
--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (8)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (8)/task.py
@@ -1,2 +1,28 @@
-#  You can experiment here, it won’t be checked
+import turtle as t
 
+
+def draw_rhombus(a, b):
+    for i in range(4):
+        t.forward(a)
+        current_angle = 180 - b if i % 2 == 1 else b
+        t.right(current_angle)
+
+
+def draw_flower_of_rhombuses():
+    for _ in range(number_rhombuses):
+        draw_rhombus(side_length, angle)
+        t.left(rotation_angle)
+
+
+side_length = 100
+angle = 60
+
+number_rhombuses = 10
+
+rotation_angle = 360 / number_rhombuses
+
+t.speed(8)
+
+draw_flower_of_rhombuses()
+
+t.exitonclick()
