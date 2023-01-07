c87dac17d14336ae0cd649115ef37ec85c0012f7
Emil Enchev <emoen4ev@gmail.com>
2023-01-07 15:43:58 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (10)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (10)/task.py
@@ -1,2 +1,19 @@
+import turtle as t
 
 
+def draw_star(a, b):
+    t.left(b)
+    for _ in range(number_vertices):
+        t.forward(a)
+        t.left(180 - b)
+
+
+side_length = 100
+number_vertices = 5
+angle = 180 / number_vertices
+
+t.speed(8)
+
+draw_star(side_length, angle)
+
+t.exitonclick()
