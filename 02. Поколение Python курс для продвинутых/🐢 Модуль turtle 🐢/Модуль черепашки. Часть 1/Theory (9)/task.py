052301295f8398a92be64e9dc856fb610021b478
Emil Enchev <emoen4ev@gmail.com>
2023-01-07 15:02:39 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (9)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 1/Theory (9)/task.py
@@ -1,2 +1,23 @@
+import turtle as t
 
 
+def draw_line(a):
+    t.forward(a)
+    t.backward(a)
+
+
+def draw_figure(a, b):
+    for _ in range(number_lines):
+        draw_line(a)
+        t.left(b)
+
+
+side_length = 100
+number_lines = 12
+angle = 360 / number_lines
+
+t.speed(8)
+
+draw_figure(side_length, angle)
+
+t.exitonclick()
