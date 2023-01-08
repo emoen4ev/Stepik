2c300c1d5fa0411ac6aa640d290a92152cd64c78
Emil Enchev <emoen4ev@gmail.com>
2023-01-08 19:39:12 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (3)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (3)/task.py
@@ -1,2 +1,31 @@
+import turtle as t
+from random import randint
 
+t.Screen().colormode(255)
+t.speed(5)
 
+number_rays = 8
+angle = 360 / number_rays
+ray_length = 150
+
+t.shape('triangle')
+
+dot_color = 'red'
+dot_size = 30
+
+for _ in range(number_rays):
+    t.forward(ray_length)
+    r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+    t.color(r, g, b)
+    t.stamp()
+    t.color('black')
+    t.penup()
+    t.backward(ray_length)
+    t.left(angle)
+    t.pendown()
+
+t.shape('classic')
+t.color(dot_color)
+t.dot(dot_size, dot_color)
+
+t.exitonclick()
