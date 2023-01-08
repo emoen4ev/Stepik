a579b4294243ecea9f9f1529229c4ab68bc7c483
Emil Enchev <emoen4ev@gmail.com>
2023-01-08 13:24:47 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (1)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (1)/task.py
@@ -1,17 +1,28 @@
 import turtle as t
+from random import randint
 
-
-t.pensize(15)
-t.penup()
-t.backward(160)
-t.pendown()
-
-for _ in range(10):
-    t.circle(7)
-    t.penup()
-    t.forward(35)
-    t.pendown()
 
 t.speed(10)
+t.Screen().colormode(255)
+
+radius = 7
+pen_with = 2 * radius
+
+number_circles = 10
+step = 55
+start_point = ((number_circles - 1) / 2) * step
+
+t.pensize(pen_with)
+t.penup()
+t.backward(start_point)
+t.pendown()
+
+for _ in range(number_circles):
+    r_g_b = tuple(randint(0, 255) for _ in range(3))
+    t.pencolor(*r_g_b)
+    t.circle(radius)
+    t.penup()
+    t.forward(step)
+    t.pendown()
 
 t.exitonclick()
