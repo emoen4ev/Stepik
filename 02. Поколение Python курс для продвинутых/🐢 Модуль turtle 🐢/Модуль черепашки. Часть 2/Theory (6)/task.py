0537a8681f7f619dd67f1c86b39373515c19066f
Emil Enchev <emoen4ev@gmail.com>
2023-01-08 22:36:57 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (6)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (6)/task.py
@@ -1,2 +1,18 @@
+import turtle as t
 
 
+t.speed(8)
+t.Screen().setup(500, 500)
+t.Screen().bgcolor('LightGreen')
+t.shape('turtle')
+t.penup()
+
+k = 1.7
+angle = 18
+
+for i in range(50):
+    t.stamp()
+    t.forward(i * k)
+    t.right(angle)
+
+t.exitonclick()
