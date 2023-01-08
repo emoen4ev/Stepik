e2ba8fca1d8d8bfd66da2eda8073f17013fad57c
Emil Enchev <emoen4ev@gmail.com>
2023-01-08 13:56:45 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (2)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (2)/task.py
@@ -1,2 +1,22 @@
+import turtle as t
+from random import randint
 
 
+t.Screen().colormode(255)
+t.speed(3)
+
+side_a_length = 150
+side_b_length = 75
+pen_size = 3
+t.pensize(pen_size)
+dot_size = 20
+angle = 90
+
+for i in range(4):
+    dot_color = randint(0, 255), randint(0, 255), randint(0, 255)
+    t.dot(dot_size, dot_color)
+    side_length = side_a_length if i % 2 == 0 else side_b_length
+    t.forward(side_length)
+    t.left(angle)
+
+t.exitonclick()
