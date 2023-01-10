8fd30ac1a176ef534c3742e6da58b4a658e6fc5c
Emil Enchev <emoen4ev@gmail.com>
2023-01-10 17:35:38 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (9)/task.py
+++ 02. Поколение Python курс для продвинутых/🐢 Модуль turtle 🐢/Модуль черепашки. Часть 2/Theory (9)/task.py
@@ -1,7 +1,6 @@
 import turtle as t
-from math import sin, pi
-
-t.speed(1)
+from math import sin, radians, cos
+from random import randint
 
 
 def draw_triangle(a, b):
@@ -12,24 +11,57 @@ def draw_triangle(a, b):
 
 def draw_star():
     for _ in range(number_triangles):
-        draw_triangle(triangle_side_length, angle)
-        t.right(transition_angle)
-        print(transition_angle)
-        t.penup()
-        t.forward(polygon_side_size)
-        t.left(transition_angle - 90)
+        t.color(get_color())
         t.pendown()
+        t.showturtle()
+        draw_triangle(triangle_side_length, angle)
+        t.penup()
+        t.hideturtle()
+        t.right(transition_angle)
+        t.forward(polygon_side_size)
+        t.left(new_starting_angle)
 
 
-triangle_side_length = 150
+def get_color():
+    r_g_b = (randint(0, 255), randint(0, 255), randint(0, 255))
+    return r_g_b
+
+
+t.speed(2)
+
+t.Screen().setup(700, 700)
+t.Screen().colormode(255)
+t.Screen().bgcolor('black')
+t.shape('turtle')
+t.pensize(3)
+
+number_triangles = 25
+triangle_side_length = 300
+
 tr_angle = 60
 angle = 180 - tr_angle
-number_triangles = 2
 number_polygon_sides = number_triangles * 3
-transition_angle = 360 / number_polygon_sides
-polygon_side_size = 2 * triangle_side_length * ((3 ** 1 / 3) / 3) * sin(pi / number_polygon_sides)
+inner_polygon_angle = 360 / number_polygon_sides
+transition_angle = (180 - inner_polygon_angle - tr_angle) / 2
+r = triangle_side_length / (2 * sin(radians(90 - (tr_angle / 2))))
+polygon_side_size = 2 * r * sin(radians(inner_polygon_angle / 2))
+new_starting_angle = 180 - (((180 - inner_polygon_angle) / 2) + tr_angle / 2)
 
-# draw_triangle(triangle_side_length, angle)
+initial_x = -triangle_side_length / 2
+initial_y = -r / 2
+
+t.penup()
+t.hideturtle()
+t.goto(initial_x, initial_y)
+
 draw_star()
+
+t.shapesize(4)
+t.setheading(90)
+t.goto(0, 0)
+t.color(get_color())
+t.stamp()
+t.color(get_color())
+t.dot()
 
 t.exitonclick()
