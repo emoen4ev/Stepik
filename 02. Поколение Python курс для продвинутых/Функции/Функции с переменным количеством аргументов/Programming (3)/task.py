06c7d7e48f33f00dfb41cdac3b592c2b34af676d
Emil Enchev <emoen4ev@gmail.com>
2023-01-16 13:46:29 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции с переменным количеством аргументов/Programming (3)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции с переменным количеством аргументов/Programming (3)/task.py
@@ -1,2 +1,9 @@
-def greet():
-    pass
+def greet(name, *args):
+    return f"Hello, {name}{('', ' and ')[1 if args else 0]}{' and '.join(current_name for current_name in args)}!"
+
+    # return f'Hello, {" and ".join((name,) + args)}!'
+
+
+# print(greet('Timur'))
+# print(greet('Timur', 'Roman'))
+# print(greet('Timur', 'Roman', 'Ruslan'))
