8b9e56927174977e5aee555af07d7a0e3a1c4f3c
Emil Enchev <emoen4ev@gmail.com>
2023-01-17 15:36:31 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (1)/task.py
@@ -1,5 +1,25 @@
-points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+def get_distance(current_point: tuple) -> float:
+    current_x_coord = current_point[0]
+    current_y_coord = current_point[1]
+
+    # return (current_x_coord ** 2 + current_y_coord ** 2) ** 1 / 2  # Если точно следовать формуле ...
+    return current_x_coord ** 2 + current_y_coord ** 2  # Этого вполне достаточно ...
 
 
+points = [
+    (-1, 1),
+    (5, 6),
+    (12, 0),
+    (4, 3),
+    (0, 1),
+    (-3, 2),
+    (0, 0),
+    (-1, 3),
+    (2, 0),
+    (3, 0),
+    (-9, 1),
+    (3, 6),
+    (8, 8),
+]
 
-print(points)
+print(sorted(points, key=get_distance))
