69c0c7619c7b6e5c03ad705bdab285bc121dcc93
Emil Enchev <emoen4ev@gmail.com>
2023-01-17 12:13:54 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Theory/task.py
@@ -1,2 +1,27 @@
-#  You can experiment here, it won’t be checked
+# def generator():
+#     def hello():
+#         print('Hello from function!')
+#     return hello
+#
+#
+# func = generator()
+# func()
+#
+# print(input)
 
+def talk(say):
+    prt = say('Мир')
+    print(prt)
+
+
+def hello(name):
+    return f'Привет {name}.'
+
+
+def goodbye(name):
+    return f'Пока {name}.'
+
+
+talk(hello)  # Привет Мир.
+
+talk(goodbye)  # Пока Мир.
