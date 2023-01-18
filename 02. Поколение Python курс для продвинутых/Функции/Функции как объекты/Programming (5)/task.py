65fbaaffb81d508f1fcd4c70506db9a46c1dc945
Emil Enchev <emoen4ev@gmail.com>
2023-01-18 15:03:02 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (5)/task.py
@@ -1,9 +1,20 @@
-def get_sum_digits(element):
-    digits = map(int, element)
-    return sum(digits)
+# def get_sum_digits(element):
+#     return sum(map(int, element))
+#
+#
+# sequence = input().split()
+#
+# sequence.sort(key=get_sum_digits)
+#
+# print(*sequence)
+
+# ------------------------ Recursive Approach ------------------------
+
+def get_sum_digits(element: int) -> int:
+    return 0 if element == 0 else element % 10 + get_sum_digits(element // 10)
 
 
-sequence = list(input().split())
+sequence = list(map(int, input().split()))
 
 sequence.sort(key=get_sum_digits)
 
