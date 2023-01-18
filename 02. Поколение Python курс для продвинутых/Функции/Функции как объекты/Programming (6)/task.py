28b40cc5b6d954e616a93d06e5412029d9171cf8
Emil Enchev <emoen4ev@gmail.com>
2023-01-18 16:01:28 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции как объекты/Programming (6)/task.py
@@ -1,1 +1,9 @@
-# put your python code here
+def get_sum_digits(element: str) -> tuple:  # -> tuple[int, int]
+    return sum(map(int, element)), int(element)
+
+
+sequence = input().split()
+
+sequence.sort(key=get_sum_digits)
+
+print(*sequence)
