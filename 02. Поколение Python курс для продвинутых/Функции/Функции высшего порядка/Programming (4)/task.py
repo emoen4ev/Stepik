44514dfd6a49b6af4bff3afdf53359bcc329fa6e
Emil Enchev <emoen4ev@gmail.com>
2023-01-19 16:06:33 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Функции высшего порядка/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Функции высшего порядка/Programming (4)/task.py
@@ -1,2 +1,19 @@
-def func_apply():
-    pass
+def func_apply(func, sequence: list) -> list:
+    result = []
+    for element in sequence:
+        result.append(func(element))
+
+    return result
+
+
+# def add3(x):
+#     return x + 3
+#
+#
+# def mul7(x):
+#     return x * 7
+#
+#
+# print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(str, [1, 2, 3, 4, 5, 6]))
