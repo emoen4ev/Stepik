0c1ff0e4cf891693a534a413312e51cff8e956ad
Emil Enchev <emoen4ev@gmail.com>
2023-01-19 18:49:49 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции map(), filter(), reduce()/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции map(), filter(), reduce()/Theory/task.py
@@ -1,2 +1,17 @@
-#  You can experiment here, it won’t be checked
+def increase(num):
+    return num + 7
 
+
+numbers = [1, 2, 3, 4, 5, 6]
+new_numbers = map(increase, numbers)
+
+print(new_numbers)
+
+new_numbers_1 = list(map(increase, numbers))
+print(new_numbers_1)
+
+new_numbers_2 = tuple(map(increase, numbers))
+print(new_numbers_2)
+
+for num in new_numbers:    #  итерируем циклом for
+    print(num)
