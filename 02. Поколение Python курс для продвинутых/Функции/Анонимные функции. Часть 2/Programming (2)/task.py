d8e6d6ae7e8024f6d7541aeec2ae5dd381c31714
Emil Enchev <emoen4ev@gmail.com>
2023-01-22 17:59:44 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (2)/task.py
@@ -1,1 +1,3 @@
-is_non_negative_num = ...
+# is_non_negative_num = lambda x: x.replace('.', '', 1).isnumeric()
+
+is_non_negative_num = lambda x: x.replace(".", "", 1).isdigit() if "." in x else x.isdigit()
