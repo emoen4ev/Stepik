3891aa409807382d41abd07769acfac6fce88126
Emil Enchev <emoen4ev@gmail.com>
2023-01-24 18:23:53 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (4)/task.py
@@ -1,1 +1,13 @@
-words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+words = [
+    'beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday',
+    'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday',
+    'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort',
+    'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside',
+    'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb',
+]
+
+filtered_words = filter(lambda word: len(word) == 6, words)
+
+sorted_words = sorted(filtered_words)
+
+print(*sorted_words)
