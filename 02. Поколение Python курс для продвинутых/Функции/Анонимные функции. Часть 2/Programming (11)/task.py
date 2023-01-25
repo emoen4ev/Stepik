f9bfaff90572590574e739fa3ccb147e194845ee
Emil Enchev <emoen4ev@gmail.com>
2023-01-25 20:12:08 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (11)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Анонимные функции. Часть 2/Programming (11)/task.py
@@ -1,1 +1,12 @@
-# put your python code here
+from functools import reduce
+
+
+def evaluate(coefficients, x):
+    n = len(coefficients) - 1
+    data = list(map(lambda y: y * (x ** (n - coefficients.index(y))), coefficients))
+    result = reduce(lambda a, b: a + b, data)
+
+    print(result)
+
+
+evaluate(list(map(int, input().split())), int(input()))
