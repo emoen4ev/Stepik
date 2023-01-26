403b5bad4baa6230ba3df6f8dc6233f85c8ec051
Emil Enchev <emoen4ev@gmail.com>
2023-01-26 18:11:45 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (2)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (2)/task.py
@@ -2,7 +2,7 @@ abscissas, ordinates, applicates = [map(float, input().split()) for _ in range(3
 
 r = 2
 
-data = list(zip(abscissas, ordinates, applicates))
+data = zip(abscissas, ordinates, applicates)
 
 result = all(map(lambda x: x[0] ** 2 + x[1] ** 2 + x[2] ** 2 <= r ** 2, data))
 
