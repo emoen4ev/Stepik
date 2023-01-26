5cc5db526b3c474d7473a77cf1fd8ac6d322457e
Emil Enchev <emoen4ev@gmail.com>
2023-01-26 17:27:38 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (1)/task.py
@@ -1,3 +1,8 @@
 countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
 capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
 population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+data = zip(capitals, countries, population)
+
+for capital, country, population in data:
+    print(f'{capital} is the capital of {country}, population equal {population} people.')
