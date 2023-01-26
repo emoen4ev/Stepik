16a9c83ccfa91213e97ec780639943f275aafd97
Emil Enchev <emoen4ev@gmail.com>
2023-01-26 17:31:24 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (1)/task.py
@@ -4,5 +4,5 @@ population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_38
 
 data = zip(capitals, countries, population)
 
-for capital, country, population in data:
-    print(f'{capital} is the capital of {country}, population equal {population} people.')
+for cur_capital, cur_country, cur_population in data:
+    print(f'{cur_capital} is the capital of {cur_country}, population equal {cur_population} people.')
