95423a134dd95d2bce5a6fe3a86dbcac2114c641
Emil Enchev <emoen4ev@gmail.com>
2023-01-26 11:38:36 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 1/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 1/Programming/task.py
@@ -1,2 +1,17 @@
-def generate_letter(mail, name, date, time, place, teacher, number):
-    pass
+def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
+    data = [
+        f'To: {mail}',
+        f'Приветствую, {name}!',
+        f'Вам назначен экзамен, который пройдет {date}, в {time}.',
+        f'По адресу: {place}.',
+        f'Экзамен будет проводить {teacher} в кабинете {number}.',
+        'Желаем удачи на экзамене!',
+    ]
+
+    return '\n'.join(data)
+
+
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
+# print()
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
+#                       'Часова 23, корпус 2', 'Василь Ярошевич', 23))
