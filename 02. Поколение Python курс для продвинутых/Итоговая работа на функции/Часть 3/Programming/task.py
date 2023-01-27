d1bc9a5753ee4b463a918f5ad16af709ece90029
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 18:47:30 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming/task.py
@@ -1,2 +1,17 @@
-def concat():
-    pass
+def concat(*args, sep=' '):
+    return sep.join(str(x) for x in args)
+
+
+# print(concat('hello', 'python', 'and', 'stepik'))
+# print(concat('hello', 'python', 'and', 'stepik', sep='*'))
+# print(concat('hello', 'python', sep='()()()'))
+# print(concat('hello', sep='()'))
+# print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
+
+'''
+hello python and stepik
+hello*python*and*stepik
+hello()()()python
+hello
+1$$2$$3$$4$$5$$6$$7$$8$$9
+'''
