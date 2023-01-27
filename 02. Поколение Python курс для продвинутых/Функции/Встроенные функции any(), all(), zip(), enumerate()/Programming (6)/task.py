b17a537c0fecf72acb822f8d31005eebdd2f4b5b
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 18:38:20 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (6)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+def help_timur(number_of_classes):
+    all_results = []
+
+    for _ in range(number_of_classes):
+        number_of_students = int(input())
+        data = [input().split() for _ in range(number_of_students)]
+        cur_class_result = any(int(x[1]) == 5 for x in data)
+        all_results.append(cur_class_result)
+
+    return ('NO', 'YES')[all(all_results)]
+
+
+print(help_timur(int(input())))
