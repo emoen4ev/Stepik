26c2581edddf36cfacbac726535f1e3faa1edea1
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 18:58:11 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (1)/task.py
@@ -1,2 +1,18 @@
+from functools import reduce
+
+
+# def product_of_odds(data):   # data - список целых чисел
+#     result = 1
+#     for i in data:
+#         if i % 2 == 1:
+#             result *= i
+#     return result
+
+
 def product_of_odds(data):
-    pass
+    filtered_data = filter(lambda x: x % 2 == 1, data)
+    result = reduce(lambda x, y: x * y, filtered_data, 1)
+    return result
+
+
+# print(product_of_odds([2, 3, 4, 5, 6, 7, 8, 9]))
