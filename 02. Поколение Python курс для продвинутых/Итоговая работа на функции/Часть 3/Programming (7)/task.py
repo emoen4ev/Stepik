707b34e386c26a720caa84a117e9aafa79b11b8d
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 20:20:58 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (7)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (7)/task.py
@@ -1,2 +1,35 @@
-def arithmetic_operation(operation):
-    pass
+def arithmetic_operation(op):
+    if op == '+':
+        return lambda x, y: x + y
+    elif op == '-':
+        return lambda x, y: x - y
+    elif op == '*':
+        return lambda x, y: x * y
+    elif op == '/':
+        return lambda x, y: x / y
+
+# ---------------------------------------------------
+
+# def arithmetic_operation(op):
+#     def operation(x, y):
+#         if op == '+':
+#             return x + y
+#         elif op == '-':
+#             return x - y
+#         elif op == '*':
+#             return x * y
+#         elif op == '/':
+#             return x / y
+#     return operation
+
+
+# add = arithmetic_operation('+')
+# div = arithmetic_operation('/')
+#
+# print(add(10, 20))
+# print(div(20, 5))
+
+'''
+30
+4.0
+'''
