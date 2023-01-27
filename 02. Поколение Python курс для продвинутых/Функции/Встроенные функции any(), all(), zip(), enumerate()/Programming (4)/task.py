27d048ccf5f860628e874841dd414103486f0fc7
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 16:44:52 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (4)/task.py
@@ -1,1 +1,14 @@
-# put your python code here
+def divisible_by_digits(a, b):
+    result = []
+    for i in range(a, b + 1):
+        if '0' in str(i):
+            continue
+        digits = [int(d) for d in str(i)]
+        if all(i % d == 0 for d in digits):
+            result.append(i)
+    return result
+
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+print(*divisible_by_digits(int(input()), int(input())))
