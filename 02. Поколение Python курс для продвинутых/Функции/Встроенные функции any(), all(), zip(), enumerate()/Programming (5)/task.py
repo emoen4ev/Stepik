8af36d5fdb383bbf96f3812977d2b69c72c563b6
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 17:27:22 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (5)/task.py
@@ -1,18 +1,36 @@
 def is_good_password(password):
-    if len(password) < 7:
-        return False
-    if not any(i.isdigit() for i in password):
-        return False
-    if not any(i.isupper() for i in password):
-        return False
-    if not any(i.islower() for i in password):
-        return False
-    return True
+    return 'YES' if all(
+        (
+            len(password) >= 7,
+            any(i.isdigit() for i in password),
+            any(i.isupper() for i in password),
+            any(i.islower() for i in password),
+        )
+    ) else 'NO'
 
 
 psw = input()
 
-if is_good_password(psw):
-    print("YES")
-else:
-    print("NO")
+print(is_good_password(psw))
+
+
+# -----------------
+
+# def is_good_password(password):
+#     if len(password) < 7:
+#         return False
+#     if not any(i.isdigit() for i in password):
+#         return False
+#     if not any(i.isupper() for i in password):
+#         return False
+#     if not any(i.islower() for i in password):
+#         return False
+#     return True
+#
+#
+# psw = input()
+#
+# if is_good_password(psw):
+#     print('YES')
+# else:
+#     print('NO')
