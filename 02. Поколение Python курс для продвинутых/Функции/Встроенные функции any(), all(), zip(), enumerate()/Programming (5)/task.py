e5a784e00295f0b73344b04e69772ee71290610e
Emil Enchev <emoen4ev@gmail.com>
2023-01-27 16:53:24 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Функции/Встроенные функции any(), all(), zip(), enumerate()/Programming (5)/task.py
@@ -1,1 +1,18 @@
-# put your python code here
+def is_good_password(password):
+    if len(password) < 7:
+        return False
+    if not any(i.isdigit() for i in password):
+        return False
+    if not any(i.isupper() for i in password):
+        return False
+    if not any(i.islower() for i in password):
+        return False
+    return True
+
+
+psw = input()
+
+if is_good_password(psw):
+    print("YES")
+else:
+    print("NO")
