c58addd6db96dd2f1f96f1c03053eac78f67ebc9
Emil Enchev <emoen4ev@gmail.com>
2023-01-28 16:44:07 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (10)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (10)/task.py
@@ -1,1 +1,8 @@
-# put your python code here
+number_ip_addresses = int(input())
+
+data = [[int(x) for x in input().split('.')] for _ in range(number_ip_addresses)]
+
+sorted_data = sorted(data, key=lambda x: x[0] * (256 ** 3) + x[1] * (256 ** 2) + x[2] * 256 + x[3])
+
+for cur_ip in sorted_data:
+    print('.'.join(str(number) for number in cur_ip))
