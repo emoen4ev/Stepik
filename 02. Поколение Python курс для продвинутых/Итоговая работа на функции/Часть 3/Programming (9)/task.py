5380189d4c7d7168704d0f596df086c9a90a4b10
Emil Enchev <emoen4ev@gmail.com>
2023-01-28 16:21:40 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (9)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на функции/Часть 3/Programming (9)/task.py
@@ -1,1 +1,18 @@
-# put your python code here
+number_words = int(input())
+
+data = [input() for _ in range(number_words)]
+
+sorted_data = sorted(data, key=lambda x: (sum([ord(a.upper()) - ord('A') for a in x]), x))
+
+print(*sorted_data, sep='\n')
+
+
+# result = []
+# for word in data:
+#     summator = 0
+#     for ch in word.upper():
+#         summator += ord(ch) - ord('A')
+#     result.append(summator)
+#
+# for i in range(len(data)):
+#     print(f'{data[i]}: {result[i]}')
