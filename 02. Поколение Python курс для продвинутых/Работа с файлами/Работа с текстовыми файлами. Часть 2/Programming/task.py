3535c331cdb453c7c7d50b2144b0671c2345948a
Emil Enchev <emoen4ev@gmail.com>
2023-01-30 14:47:09 +0200

    Added solutions.

---
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 2/Programming/task.py
@@ -0,0 +1,10 @@
+with open('text.txt', 'rt', encoding='utf-8') as file:
+    print(file.read()[::-1])
+
+'''
+Read secret_message.txt and print out the first letter of each line.
+'''
+
+'''
+.enil hcae fo rettel tsrif eht tuo tnirp dna txt.egassem_terces daeR
+'''
