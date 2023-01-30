48888403a4a28d952262bb150446f98eed437e9c
Emil Enchev <emoen4ev@gmail.com>
2023-01-30 20:27:36 +0200

    Added solutions.

---
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 2/Programming (2)/task.py
@@ -0,0 +1,33 @@
+with open('lines.txt', 'rt', encoding='utf-8') as file:
+    data = file.readlines()
+    sorted_by_length = sorted(data, key=lambda line: -len(line))
+    greatest_length = len(sorted_by_length[0])
+    result= list(filter(lambda line: greatest_length, sorted_by_length))
+
+print(result)
+
+
+
+
+'''
+future belongs
+to those who
+believe in their..
+dreams
+Life begins
+at the end
+of your comfort
+zone
+Life is what 12345
+happens while you
+are busy making
+other plans
+Beautiful is
+better than ugly!!
+Explicit is better
+than implicit
+Simple is better
+than complex
+Complex is better
+than complicated
+'''
