39e5c27e630df19731e49cc3aa5def515a725fa6
Emil Enchev <emoen4ev@gmail.com>
2023-02-01 19:26:13 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Theory/task.py
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Theory/task.py
@@ -1,2 +1,22 @@
-#  You can experiment here, it won’t be checked
+with open('books.txt', 'w', encoding='utf-8') as file:
+    li = '''Лев Толстой «Война и мир», Джордж Оруэлл «1984», Джеймс Джойс «Улисс», Владимир Набоков «Лолита», Уильям Фолкнер «Звук и ярость»'''.split(',')
+    print(li)
+    li2 = []
+    for i in li:
+        print(i)
+        string = f"{i.strip()}\n" #'{}\n'.format(i.strip())
+        li2.append(string)
+    file.writelines(li2)
 
+with open('books.txt', 'w', encoding='utf-8') as file:
+    li = '''Лев Толстой «Война и мир», Джордж Оруэлл «1984», Джеймс Джойс «Улисс», Владимир Набоков «Лолита», Уильям Фолкнер «Звук и ярость»'''.split(',')
+    file.writelines([f"{i.strip()}\n" for i in li])
+
+with open('books.txt', 'w', encoding='utf-8') as file:
+    li = '''Лев Толстой «Война и мир», Джордж Оруэлл «1984», Джеймс Джойс «Улисс», Владимир Набоков «Лолита», Уильям Фолкнер «Звук и ярость»'''.split(',')
+    for i in li:
+        print(i.strip(), file=file)
+
+with open('file.txt', 'w', encoding='utf-8') as output:
+    li = 'Лев Толстой «Война и мир», Джордж Оруэлл «1984», Джеймс Джойс «Улисс», Владимир Набоков «Лолита», Уильям Фолкнер «Звук и ярость»'.split(', ')
+    print(*li, sep='\n', file=output)
