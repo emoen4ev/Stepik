77d5271f3b2e814fb2d5d4b9bc2be34ad4943a1b
Emil Enchev <emoen4ev@gmail.com>
2023-02-02 10:37:17 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (1)/task.py
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (1)/task.py
@@ -1,1 +1,13 @@
-# put your python code here
+from random import choices
+
+
+with open('random.txt', 'w', encoding='utf-8') as output_file:
+    random_data = choices(range(111, 778), k=25)
+    data_for_writing = '\n'.join(str(el) for el in random_data)
+    output_file.writelines(data_for_writing)
+
+    # print(*random_data, sep='\n', file=output_file)
+
+# with open('random.txt', 'rt', encoding='utf-8') as file:
+#     data = file.readlines()
+# print(data)
