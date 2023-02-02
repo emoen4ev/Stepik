05b94ae0500a55bb972d7f893a4fb21118402a3a
Emil Enchev <emoen4ev@gmail.com>
2023-02-02 18:53:52 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (4)/task.py
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (4)/task.py
@@ -1,1 +1,23 @@
-# put your python code here
+colors = []
+goats = []
+with open("goats_1.txt", "r") as f:
+    lines = f.readlines()
+    print(lines)
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if line == "COLOURS":
+            colors = [x.strip() for x in lines[i+1:]]
+        elif line == "GOATS":
+            goats = [x.strip() for x in lines[i+1:]]
+    print(colors)
+    print(goats)
+
+color_count = {}
+for color in colors:
+    color_count[color] = goats.count(color)
+print(color_count)
+
+with open("answer.txt", "w") as f:
+    for color, count in sorted(color_count.items()):
+        if count > len(goats) * 0.07:
+            f.write(f"{color}\n")
