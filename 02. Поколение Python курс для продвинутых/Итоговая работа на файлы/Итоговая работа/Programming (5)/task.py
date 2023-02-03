3eb5ec8bf5ad092bc28d64e660c553e8dec99382
Emil Enchev <emoen4ev@gmail.com>
2023-02-03 15:37:41 +0200

    Course updated.

--- 02. Поколение Python курс для продвинутых/Итоговая работа на файлы/Итоговая работа/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Итоговая работа на файлы/Итоговая работа/Programming (5)/task.py
@@ -1,2 +1,14 @@
-#  write your answer here 
+# file_name = input()
+file_name = 'test_file.txt'
 
+with open('forbidden_words_1.txt', 'rt', encoding='utf-8') as input_file:
+    forbidden_words = [word.strip().split() for word in input_file][0]
+
+    print(forbidden_words)
+
+with open(file_name, 'rt', encoding='utf-8') as working_file:
+    for line in working_file:
+        for word in forbidden_words:
+            if word in line.lower():
+                new_line = line.lower().replace(word, '*' * len(word))
+            print(new_line.strip())
