e887ce1450156a4e276483aa91bcf3cac82d7434
Emil Enchev <emoen4ev@gmail.com>
2023-02-03 11:22:39 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (5)/task.py
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (5)/task.py
@@ -1,1 +1,8 @@
-# put your python code here
+number_files = int(input())
+files = [input() for _ in range(number_files)]
+
+with open('output.txt', 'at', encoding='utf-8') as output_file:
+    for current_file in files:
+        with open(current_file, 'rt', encoding='utf-8') as input_file:
+            input_data = input_file.read()
+            output_file.write(input_data)
