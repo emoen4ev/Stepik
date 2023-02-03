8eeb3aff3a1ed5e3ac3b72243b212e88a18d8c67
Emil Enchev <emoen4ev@gmail.com>
2023-02-03 12:38:51 +0200

    Added solutions.

--- 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (6)/task.py
+++ 02. Поколение Python курс для продвинутых/Работа с файлами/Работа с текстовыми файлами. Часть 3/Programming (6)/task.py
@@ -1,1 +1,20 @@
-# put your python code here
+max_time_in_minutes = 59
+output_data = []
+
+with open('logfile.txt', 'rt', encoding='utf-8') as input_file:
+    data = [el.strip().split(', ') for el in input_file]
+    for current_data in data:
+        name, start, end = current_data[0], [int(x) for x in current_data[1].split(':')], [int(x) for x in
+                                                                                           current_data[2].split(':')]
+
+        remaining_time = (end[0] * 60 + end[1]) - (start[0] * 60 + start[1])
+
+        if remaining_time > max_time_in_minutes:
+            output_data.append(name)
+
+        # remaining_time > max_time_in_minutes and output_data.append(name)
+
+result = '\n'.join(output_data)
+
+with open('output.txt', 'wt', encoding='utf-8') as output_file:
+    output_file.write(result)
