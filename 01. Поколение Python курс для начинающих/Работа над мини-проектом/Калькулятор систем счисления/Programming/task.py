de5420dd9ccabdba1b3f9d318129d85442e72cd1
Emil Enchev <emoen4ev@gmail.com>
2023-02-05 18:00:34 +0200

    Added solutions.

--- 01. Поколение Python курс для начинающих/Работа над мини-проектом/Калькулятор систем счисления/Programming/task.py
+++ 01. Поколение Python курс для начинающих/Работа над мини-проектом/Калькулятор систем счисления/Programming/task.py
@@ -1,1 +1,7 @@
-# put your python code here
+number = int(input())
+
+b = bin(number)[2:]
+o = oct(number)[2:]
+h = hex(number)[2:].upper()
+
+print(b, o, h, sep='\n')
--- 01. Поколение Python курс для начинающих/Работа над мини-проектом/Калькулятор систем счисления/Theory (3)/task.py
+++ 01. Поколение Python курс для начинающих/Работа над мини-проектом/Калькулятор систем счисления/Theory (3)/task.py
@@ -1,2 +1,11 @@
-#  You can experiment here, it won’t be checked
+print(bin(513))
+print(oct(513))
+print(hex(513))
 
+print(bin(513)[2:])
+print(oct(513)[2:])
+print(hex(513)[2:])
+
+print(hex(765879))
+print(hex(765879)[2:])
+print(hex(765879)[2:].upper())
