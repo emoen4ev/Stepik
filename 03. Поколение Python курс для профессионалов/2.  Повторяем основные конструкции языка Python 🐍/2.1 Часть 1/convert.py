50d9692ccd74520bb04f74730c2771b7facd2c34
Emil Enchev <emoen4ev@gmail.com>
2023-02-08 11:18:55 +0200

    Added solution for task convert in project.

---
+++ 03. Поколение Python курс для профессионалов/2.  Повторяем основные конструкции языка Python 🐍/2.1 Часть 1/convert.py
@@ -0,0 +1,34 @@
+"""
+---------- task / requirements --------
+Функция convert()
+
+Реализуйте функцию convert(), которая принимает один аргумент:
+- string — произвольная строка
+
+Функция должна возвращать строку string:
+- полностью в нижнем регистре, если букв в нижнем регистре в этой строке больше
+- полностью в верхнем регистре, если букв в верхнем регистре в этой строке больше
+- полностью в нижнем регистре, если количество букв в верхнем и нижнем регистрах в этой строке совпадает
+
+Примечание 1. Символы строки, не являющиеся буквами, следует игнорировать.
+"""
+
+
+def convert(text: str):
+    lowercase_counter = sum([x.islower() for x in text])
+    uppercase_counter = sum([x.isupper() for x in text])
+
+    return text.upper() if uppercase_counter > lowercase_counter else text.lower()
+
+
+# ------- test inputs -----
+print(convert('BEEgeek'))
+print(convert('pyTHON'))
+print(convert('pi31415!'))
+
+'''
+--------- expected outputs --------
+beegeek
+PYTHON
+pi31415!
+'''
