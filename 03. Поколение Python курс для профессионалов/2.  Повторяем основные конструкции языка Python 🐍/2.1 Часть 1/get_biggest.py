5c685f6ae7cea7fc3cb1e06eaaa314e193afcaae
Emil Enchev <emoen4ev@gmail.com>
2023-02-09 18:55:17 +0200

    Added more test data.

--- 03. Поколение Python курс для профессионалов/2.  Повторяем основные конструкции языка Python 🐍/2.1 Часть 1/get_biggest.py
+++ 03. Поколение Python курс для профессионалов/2.  Повторяем основные конструкции языка Python 🐍/2.1 Часть 1/get_biggest.py
@@ -19,7 +19,7 @@ def get_biggest(numbers: list) -> int:
     if not numbers:
         return -1
 
-    sorted_numbers = sorted(map(str, numbers), key=lambda x: x * 4, reverse=True)
+    sorted_numbers = sorted(map(str, numbers), key=lambda x: x * len(str(max(numbers))), reverse=True)
 
     return int(''.join(sorted_numbers))
 
@@ -30,6 +30,8 @@ print(get_biggest([61, 228, 9, 3, 11]))
 print(get_biggest([7, 71, 72]))
 print(get_biggest([]))
 print(get_biggest([7, 71, 72, 8]))
+print(get_biggest([9, 91, 92, 8]))
+print(get_biggest([13, 2211, 423, 53, 1, 2, 33, 58, 78554, 34, 65, 65, 2, 1]))
 '''
 --------- expected outputs --------
 321
@@ -37,4 +39,6 @@ print(get_biggest([7, 71, 72, 8]))
 77271
 -1
 877271
+992918
+785546565585342334332222111311
 '''
