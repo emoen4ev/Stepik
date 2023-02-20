4ab11d3a7d2def9bf640b33803295d15cc15fe80
Emil Enchev <emoen4ev@gmail.com>
2023-02-20 17:53:31 +0200

    Added 'print_current_date_in_iso_format' task.

---
+++ 03. Поколение Python курс для профессионалов/3.  Работа с датой и временем/3.1 Типы данных date и time. Часть 1/print_current_date_in_iso_format.py
@@ -0,0 +1,16 @@
+"""
+Дополните приведенный ниже код, чтобы он вывел текущую дату в ISO формате (YYYY-MM-DD).
+
+# импортируем тип date из модуля datetime
+from ____ import ____
+
+# выводим текущую дату
+print(____)
+
+"""
+
+from datetime import date
+
+current_date = date.today()
+
+print(current_date)
