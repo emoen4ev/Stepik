15f20a7bc2423a93c7bf0f14068bf52d900fb788
Emil Enchev <emoen4ev@gmail.com>
2023-02-20 14:36:49 +0200

    Added solution to 'files_in_a_file' task.

--- 03. Поколение Python курс для профессионалов/2.  Повторяем основные конструкции языка Python 🐍/2.2 Часть 2/files_in_a_file.py
+++ 03. Поколение Python курс для профессионалов/2.  Повторяем основные конструкции языка Python 🐍/2.2 Часть 2/files_in_a_file.py
@@ -73,9 +73,62 @@ Summary: 1 GB
 При открытии файла используйте явное указание кодировки UTF-8.
 """
 
+
+def get_size_in_bytes(value: int, dimension: str) -> float:
+    if dimension in CONVERTING_TABLE:
+        return value * CONVERTING_TABLE[dimension]
+
+
+def get_corrected_size_and_dimension(value: float, dimension: str) -> (int, str):
+    idx = UNITS.index(dimension)
+    while value > 1023 and idx < 5:
+        value /= 1024
+        idx += 1
+    return round(value), UNITS[idx]
+
+
+data = {}
+
+CONVERTING_TABLE = {
+    'KB': 1024,
+    'MB': 1024 ** 2,
+    'GB': 1024 ** 3,
+    'TB': 1024 ** 4,
+}
+
+UNITS = ('B', 'KB', 'MB', 'GB', 'TB')
+
 with open('files.txt', 'rt', encoding='utf-8') as file:
-    line = file.readline().split()
-    print(line)
+    for line in file:
+        line = line.split()
+        name = line[0]
+        extension = line[0].split('.')[1]
+        size, unit = int(line[1]), line[2]
+        data.setdefault(extension, (),)
+        data[extension] += (name, size, unit),
+
+for k, v in sorted(data.items()):
+    v = sorted(v, key=lambda x: x[0])
+    sum_sizes_in_bytes = 0
+    dimensions = set()
+    for el in v:
+        el_name = el[0]
+        el_size = el[1]
+        el_dimension = el[2]
+        dimensions.add(el_dimension)
+        if el_dimension != 'B':
+            el_size = get_size_in_bytes(el_size, el_dimension)
+        sum_sizes_in_bytes += el_size
+        print(el_name)
+    largest_dimension = ('GB' in dimensions and 'GB') or ('TB' in dimensions and 'TB') \
+        or ('MB' in dimensions and 'MB') or ('KB' in dimensions and 'KB') or ('B' in dimensions and 'B')
+    sum_sizes_in_largest_dimension = sum_sizes_in_bytes / CONVERTING_TABLE[largest_dimension]
+    if sum_sizes_in_largest_dimension > 1023:
+        sum_sizes_in_largest_dimension, largest_dimension = get_corrected_size_and_dimension(
+            sum_sizes_in_largest_dimension, largest_dimension)
+
+    print('----------')
+    print(f'Summary: {round(sum_sizes_in_largest_dimension)} {largest_dimension}\n')
 
 '''
 ------- test inputs -----
