ef8ae116dac4ab5e5f84d030dad30c81791ed5cc
Emil Enchev <emoen4ev@gmail.com>
2023-02-22 10:01:02 +0200

    Added solution to 'two_dates' task.

--- 03. Поколение Python курс для профессионалов/3.  Работа с датой и временем/3.2 Типы данных date и time. Часть 2/two_dates.py
+++ 03. Поколение Python курс для профессионалов/3.  Работа с датой и временем/3.2 Типы данных date и time. Часть 2/two_dates.py
@@ -7,3 +7,30 @@
 Формат выходных данных:
 Программа должна выбрать из двух введенных дат меньшую и вывести ее в формате DD-MM (YYYY).
 """
+
+from datetime import date
+
+
+date_1, date_2 = date.fromisoformat(input()), date.fromisoformat(input())
+
+smallest_date = min(date_1, date_2)
+
+print(smallest_date.strftime('%d-%m (%Y)'))
+
+'''
+Sample Input 1:
+2021-05-12
+2021-05-04
+
+Sample Output 1:
+04-05 (2021)
+
+----------------------------------------------------------------
+
+Sample Input 2:
+1999-07-14
+1999-07-14
+
+Sample Output 2:
+14-07 (1999)
+'''
